d5d4ce773a64a6d882f37f265432172b2235b7ee
JeonghoPark <104881579+d0lbangi@users.noreply.github.com>
2023-08-09 23:52:28 +0900

    Update Test4-3.sol

--- Chapter4/Test4-3.sol
+++ Chapter4/Test4-3.sol
@@ -6,10 +6,10 @@ contract Test4_3 {
 
     function fun1() public pure returns(uint) {
         uint result = 0;
-        uint a = 10;
+        uint a = 12;
 
         while(true) {
-            if(a==5) {
+            if(a==7) {
                 break;
             }
             result = result + a;
