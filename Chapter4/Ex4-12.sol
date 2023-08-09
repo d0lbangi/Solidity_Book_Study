cc296be57fdbff80c9a1b105324ec758fc8fd6eb
JeonghoPark <104881579+d0lbangi@users.noreply.github.com>
2023-08-09 23:51:28 +0900

    Update Ex4-12.sol

--- Chapter4/Ex4-12.sol
+++ Chapter4/Ex4-12.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.18;
 contract Ex4_12 {
     
     function fun1() public pure returns(uint) {
-        uint result = 0;
+        uint result = 0; // 변수 선언 및 초기화
         for(uint a = 0; a<2; a++) {
             if(a==1) {
                 continue;
